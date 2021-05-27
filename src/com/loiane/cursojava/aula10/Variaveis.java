f6c60884764072886e1697d50a13d228da43330f
JulianaOnofrio <julianaonofrio@hotmail.com>
2021-05-27 01:28:53 -0300

    Aula 10

---
+++ src/com/loiane/cursojava/aula10/Variaveis.java
@@ -0,0 +1,18 @@
+package com.loiane.cursojava.aula10;
+
+public class Variaveis {
+
+    public static void main(String[] args) {
+        //convenção Java
+        int idade = 20;
+        String nome = "Juliana";
+        String nomeDoMeuCachorro ="Bach";
+        String ano2021;
+
+        idade = 29;
+
+        System.out.println("Idade = " + idade);
+        System.out.println("Nome = " + nome);
+
+    }
+}
